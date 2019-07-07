6ed57ae97b55b03e6c992ffc892bb03607766c71
Micah Snyder <micasnyd@cisco.com>
2019-07-07 16:50:47 -0400

    Adds openssl recipe for Darwin.

---
+++ recipes/Darwin/openssl/1.1.0j/openssl__1_1_0j.py
@@ -0,0 +1,48 @@
+"""
+Copyright (C) 2019 Cisco Systems, Inc. and/or its affiliates. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+import os
+
+from recipes.recipe import BaseRecipe
+
+
+class Recipe(BaseRecipe):
+    """
+    Recipe to build libssl.
+    """
+
+    name = "openssl"
+    version = "1.1.0j"
+    url = "https://www.openssl.org/source/openssl-1.1.0j.tar.gz"
+    install_paths = {
+        "host": {
+            "include": [os.path.join("include", "openssl")],
+            "lib": [
+                os.path.join("libssl.1.1.dylib"),
+                os.path.join("libssl.a"),
+                os.path.join("libcrypto.1.1.dylib"),
+                os.path.join("libcrypto.a"),
+            ],
+        }
+    }
+    dependencies = ["zlib"]
+    required_tools = ["make", "clang"]
+    build_script = {
+        "host": """
+            ./config zlib --with-zlib-include="{includes}" --with-zlib-lib="{libs}"
+            make
+        """
+    }
