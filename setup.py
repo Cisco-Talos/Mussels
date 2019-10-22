6b76fde5747f76cd87be3132fa75384c5bd26a64
Micah Snyder <micasnyd@cisco.com>
2019-10-21 22:58:28 -0400

    Fixed mussels setup.py package qualifier.

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setuptools.setup(
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: Apache 2.0 License",
+        "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
     ],
 )
